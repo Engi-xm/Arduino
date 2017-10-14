cfc3d184223e372c071c087b8411bde4c489611a
Engi-xm <kristijonas.a@gmail.com>
2017-10-14 14:54:35 +0300

    initial upload

---
+++ br_35_test/br_35_test.ino
@@ -0,0 +1,198 @@
+#include <SPI.h>
+#include <SD.h>
+#include <Wire.h>
+
+#define RTC_ADDR 0b1101000
+
+struct time_buf {
+  uint8_t secs;
+  uint8_t mins;
+  uint8_t hrs;
+  uint8_t day;
+  uint8_t mnth;
+};
+struct temp_buf {
+  uint8_t temp1 = 0;
+  uint8_t temp2 = 0;
+};
+
+void record_to_sd(temp_buf* t1, time_buf* t2); // record time and temperature
+void read_rtc(time_buf* buf); // read time
+uint8_t bcd_to_dec(uint8_t a); // convert bcd to dec
+uint8_t read_temp(uint8_t adc_ch); // read temperature from thermistor on adc channel
+
+// thermistor lookup R1=9.95K Rt=100k@25C
+uint16_t temp_lookup[][2] = { 
+  {975, 10}, // {ADC value, temperature}
+  {963, 15},
+  {948, 20},
+  {930, 25},
+  {911, 30},
+  {888, 35},
+  {863, 40},
+  {834, 45},
+  {801, 50},
+  {768, 55},
+  {732, 60},
+  {693, 65},
+  {653, 70},
+  {611, 75},
+  {570, 80},
+  {529, 85},
+  {489, 90},
+  {449, 95},
+  {412, 100},
+  {379, 105},
+  {345, 110},
+  {314, 115},
+  {285, 120},
+  {257, 125}
+};
+
+const uint8_t chip_select = 4; // cc for sd card (not used)
+const uint8_t machine_control = 7; // machine control pin (high: machine runs)
+const uint8_t overheat_led = 5; // overheat indicator pin
+const uint8_t write_err_led = 6; // write error indicator pin
+const uint8_t therm1_pin = A1; // thermistor pin
+const uint8_t therm2_pin = A2; // thermistor pin
+const uint8_t temp_limit = 100; // max motor temp
+const uint32_t interval = 6000; // interval for measurements
+uint8_t interval_iter = 0; // interval iterator
+uint32_t prev_millis, curr_millis; // timing helpers
+time_buf time_buffer; // time buffer
+temp_buf temp_buffer; // temp buffer
+
+void setup() {
+  Serial.begin(9600); // open serial port
+  Wire.begin(); // start i2c bus
+
+  SD.begin(chip_select); // initialize sd card
+ 
+  analogReference(EXTERNAL); // set aref to 3.3V
+
+  pinMode(machine_control, OUTPUT);
+  pinMode(overheat_led, OUTPUT);
+  pinMode(write_err_led, OUTPUT);
+
+  digitalWrite(machine_control, 0);
+  digitalWrite(overheat_led, 0);
+  digitalWrite(write_err_led, 0);
+}
+
+void loop() {
+  // check time
+  curr_millis = millis();
+
+  // if interval reached
+  if(curr_millis - prev_millis >= interval) {
+    // record time
+    prev_millis = curr_millis;
+    
+    // measure temp
+    temp_buffer.temp1 = read_temp(therm1_pin);
+    temp_buffer.temp2 = read_temp(therm2_pin);
+    
+    // if overheating, rest
+    if(temp_buffer.temp1 >= temp_limit ||
+       temp_buffer.temp2 >= temp_limit) {
+      record_to_sd(&temp_buffer, &time_buffer);
+      digitalWrite(overheat_led, 1); // turn on indicator led
+      delay(60000);
+      record_to_sd(&temp_buffer, &time_buffer);
+    }
+    
+    // control machine
+    ++interval_iter <= 9 ? 
+      digitalWrite(machine_control, 1) : 
+      digitalWrite(machine_control, 0);
+
+    // record time and temp every 5th interval
+    if(!(interval_iter % 5)) {
+      read_rtc(&time_buffer);
+      record_to_sd(&temp_buffer, &time_buffer);
+    }
+
+    // reset iteration
+    interval_iter == 10 ? interval_iter = 0 : 0;
+  }
+}
+
+void record_to_sd(temp_buf* t1, time_buf* t2) {
+  // TODO: smth  
+  File data_file = SD.open("datalog.txt", FILE_WRITE);
+  
+  // if the file is available, write to it:
+  if (data_file) {
+    data_file.print(t2->mnth);
+    data_file.print('-');
+    data_file.print(t2->day);
+    data_file.print("\t");
+    data_file.print(t2->hrs);
+    data_file.print(':');
+    data_file.print(t2->mins);
+    data_file.print(':');
+    data_file.print(t2->secs);
+    data_file.print("\t");
+    data_file.print(t1->temp1);
+    data_file.print("\t");
+    data_file.println(t1->temp2);
+    data_file.close();
+  } else {
+    digitalWrite(write_err_led, 1); // turn on indicator led
+  }
+}
+
+uint8_t bcd_to_dec(uint8_t a) {
+  return(a - 6 * (a >> 4));
+}
+
+void read_rtc(time_buf* buf) {
+  // TODO: works
+  uint8_t loc_buf[6];
+  
+  // set address to seconds
+  Wire.beginTransmission(RTC_ADDR);
+  Wire.write(0);
+  Wire.endTransmission();
+
+  Wire.requestFrom(RTC_ADDR, 6); // request information
+  
+  // read information
+  for(uint8_t i = 0; i < 6; i++) {
+    loc_buf[i] = Wire.read(); // receive byte
+  }
+
+  // copy to buffer
+  buf->secs = bcd_to_dec(loc_buf[0]);
+  buf->mins = bcd_to_dec(loc_buf[1]);
+  buf->hrs = bcd_to_dec(loc_buf[2]);
+  buf->day = bcd_to_dec(loc_buf[4]);
+  buf->mnth = bcd_to_dec(loc_buf[5]);
+}
+
+uint8_t read_temp(uint8_t adc_ch) {
+  uint16_t adc_val;
+  uint8_t i = 0;
+  uint16_t adc_val0, adc_val1; // values for linear interpolation
+  uint8_t temp0, temp1; // values for linear interpolation
+  
+  adc_val = analogRead(adc_ch);
+
+  // search for adc_value
+  while(adc_val < temp_lookup[i][0]) {
+    i++;
+  }
+
+  // lookup values
+  if(i) {
+    adc_val0 = temp_lookup[i-1][0];
+    adc_val1 = temp_lookup[i][0];
+    temp0 = temp_lookup[i-1][1];
+    temp1 = temp_lookup[i][1];
+  } else return(0);
+
+  // return interpolated temperature
+  return((temp0 * (adc_val - adc_val1) + temp1 * (adc_val0 - adc_val)) / 
+         (adc_val0 - adc_val1));
+}
+
