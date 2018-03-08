18a0998e291a6e177cef4a749e1eb8165bcd2cdb
Engi-xm <kristijonas.a@gmail.com>
2018-03-08 11:36:55 +0200

    update for blockade test

--- br_35_test/br_35_test.ino
+++ br_35_test/br_35_test.ino
@@ -4,15 +4,19 @@
 
 #define RTC_ADDR 0b1101000 // rtc i2c address
 #define HALL_INT 2 // hall effect sensor pin for interrupts
-#define RTC_SELECT 4 // cc for sd card (not used)
+#define ERR_LED 3 // error indicator pin
+#define EXTEND_RELAY 5 // piston extend relay pin
+#define RETRACT_RELAY 6 // piston retract relay pin
 #define MACHINE_CTRL 7 // machine control pin (high: machine runs)
-#define OVERHEAT_LED 5 // overheat indicator pin
+#define RETRACT_ENDSTOP 8 // piston endstop retracted position pin
+#define EXTEND_ENDSTOP 9 // piston endstop end position pin
 #define OVERHEAT_REST_INTERVAL 300000 // 5mins
-#define WRITE_ERR_LED 6 // write error indicator pin
 #define THERM_PIN A1 // thermistor pin
 #define CURR_PIN A3 // current sensor pin
 #define TEMP_LIMIT 100 // max motor temp
-#define INTERVAL 4000 // INTERVAL for measurements
+#define CURR_LIMIT 200 // max current (dA)
+#define RPM_LIMIT 600 // min brush rpm on free rotation
+#define INTERVAL 200 // INTERVAL for measurements
 #define CURR_CALIBRATION 508 // value for centering adc value
 #define CYCLE_MAX 55000 // number of cycles to run
 
@@ -26,6 +30,9 @@ struct time_buf {
 
 void record_to_sd(uint8_t temp, uint16_t rpm, uint8_t current); // record time, current, rpm and temperature
 void read_rtc(time_buf* buf); // read time
+void retract(uint8_t* piston_status); // retract cylinder routine
+void extend(uint8_t* piston_status); // extend cylinder routine
+void error(uint8_t code); // error routine (0 overheat, 1 overcurrent, 2 low rpm, 3 sd card)
 uint8_t bcd_to_dec(uint8_t a); // convert bcd to dec
 uint8_t read_temp(uint8_t adc_ch); // read temperature from thermistor on adc channel
 uint8_t read_current(uint8_t adc_ch); // read current from sensor on adc channel
@@ -62,33 +69,40 @@ uint16_t temp_lookup[][2] = {
 uint8_t interval_iter = 0; // interval iterator
 uint8_t temp_buffer = 0; // temp buffer
 uint8_t current_buffer = 0; // current buffer
+uint8_t piston_status = 0; // piston status flag (0 retracted, 1 extended)
 uint16_t rpm_buffer = 0; // rpm buffer
 uint16_t cycle_iter = 0; // number of cycles passed
 uint32_t rpm_iter = 0; // rpm iterator
-uint32_t prev_millis, curr_millis; // timing helpers
+uint32_t prev_millis, curr_millis; // timing variables
 
 void setup() {
   Serial.begin(9600); // open serial port
   Wire.begin(); // start i2c bus
 
-  SD.begin(RTC_SELECT); // initialize sd card
+  // initialize sd card
+  if(!SD.begin()) error(3); 
  
   attachInterrupt(digitalPinToInterrupt(HALL_INT), hall_interrupt, FALLING);
 
   pinMode(MACHINE_CTRL, OUTPUT);
-  pinMode(OVERHEAT_LED, OUTPUT);
-  pinMode(WRITE_ERR_LED, OUTPUT);
+  pinMode(ERR_LED, OUTPUT);
+  pinMode(EXTEND_RELAY, OUTPUT);
+  pinMode(RETRACT_RELAY, OUTPUT);
+  
+  pinMode(RETRACT_ENDSTOP, INPUT);
+  pinMode(EXTEND_ENDSTOP, INPUT);
 
   digitalWrite(MACHINE_CTRL, 0);
-  digitalWrite(OVERHEAT_LED, 0);
-  digitalWrite(WRITE_ERR_LED, 0);
+  digitalWrite(ERR_LED, 0);
+  digitalWrite(EXTEND_RELAY, 0);
+  digitalWrite(RETRACT_RELAY, 0);
 }
 
 void loop() {
   // check time
   curr_millis = millis();
 
-  if(curr_millis - prev_millis >= INTERVAL) { // if INTERVAL reached (every 4s)
+  if(curr_millis - prev_millis >= INTERVAL) { // if INTERVAL reached (every .2s)
     // record time
     prev_millis = curr_millis;
 
@@ -99,37 +113,53 @@ void loop() {
       // if overheating, rest
       if(temp_buffer >= TEMP_LIMIT) {
         record_to_sd(temp_buffer, 0, 0);
-        digitalWrite(OVERHEAT_LED, 1); // turn on indicator led
+        error(0);
         digitalWrite(MACHINE_CTRL, 0); // turn off machine
         delay(OVERHEAT_REST_INTERVAL);
         record_to_sd(temp_buffer, 0, 0);
         interval_iter = 0;
       }
+
+      // check if running, start if not
+      digitalRead(MACHINE_CTRL) ? void() : digitalWrite(MACHINE_CTRL, 1);
       
       // control machine
-      ++interval_iter <= 14 ? 
-        digitalWrite(MACHINE_CTRL, 1) : // every 56s
-        digitalWrite(MACHINE_CTRL, 0); // every 4s
+      ++interval_iter <= 40 ? 
+        retract(&piston_status) : // 8s/10s
+        extend(&piston_status); // 2s/10s
 
-      if(interval_iter == 1 || interval_iter == 8) {
+      // start rpm measurement
+      if(interval_iter == 1 || interval_iter == 45) {
         read_rpm(0); // start rpm measurement
       }
   
-      // record info on 7th (28s) and 14th interval (56s)
-      if(interval_iter == 7 || interval_iter == 14) {
-        rpm_buffer = read_rpm(1); // record rpm
+      // record and check info on 30th (6s) and 50th interval (10s)
+      if(interval_iter == 30 || interval_iter == 50) {
+        // record
         current_buffer = read_current(CURR_PIN); // record current
+        rpm_buffer = read_rpm(1); // record rpm
         record_to_sd(temp_buffer, rpm_buffer, current_buffer);
+
+        // check
+        if(current_buffer >= CURR_LIMIT) {
+          error(1);
+        }
+        if(interval_iter <= 40 && rpm_buffer <= RPM_LIMIT) {
+          error(2);
+        }
       }
   
-      if(interval_iter == 15) { // every 60s
+      if(interval_iter == 50) { // every 10s
         interval_iter = 0; // reset iteration
         cycle_iter++; // iterate cycles
       }
+    } else {
+      digitalWrite(MACHINE_CTRL, 0); // if finished, turn machine off
     }
   }
 
 //  // SANITY CHECK
+//  digitalWrite(MACHINE_CTRL, 1);
 //  read_rpm(0);
 //  delay(5000);
 //  Serial.print(read_temp(THERM_PIN));
@@ -137,7 +167,8 @@ void loop() {
 //  Serial.print(read_current(CURR_PIN));
 //  Serial.print("\t");
 //  Serial.println(read_rpm(1));
-//  delay(500);
+//  digitalWrite(MACHINE_CTRL, 0);
+//  delay(2000);
 }
 
 void record_to_sd(uint8_t temp, uint16_t rpm, uint8_t current) {
@@ -166,7 +197,9 @@ void record_to_sd(uint8_t temp, uint16_t rpm, uint8_t current) {
     data_file.println(current);
     data_file.close();
   } else {
-    digitalWrite(WRITE_ERR_LED, 1); // turn on indicator led
+    while(1) {
+      error(3);
+    }
   }
 }
 
@@ -196,6 +229,60 @@ void read_rtc(time_buf* buf) {
   buf->hrs = bcd_to_dec(loc_buf[2]);
   buf->day = bcd_to_dec(loc_buf[4]);
   buf->mnth = bcd_to_dec(loc_buf[5]);
+}
+
+void retract(uint8_t* piston_status) {
+  if(piston_status) {
+    digitalWrite(RETRACT_RELAY, 1);
+    while(digitalRead(RETRACT_ENDSTOP));
+    digitalWrite(RETRACT_RELAY, 0);
+    *piston_status = 0;
+  }
+}
+
+void extend(uint8_t* piston_status) {
+  if(!piston_status) {
+    digitalWrite(EXTEND_RELAY, 1);
+    while(digitalRead(EXTEND_ENDSTOP));
+    digitalWrite(EXTEND_RELAY, 0);
+    *piston_status = 1;
+  }
+}
+
+void error(uint8_t code) {
+  switch(code) {
+    case 0: // overheat
+      digitalWrite(ERR_LED, 1);
+      break;
+    case 1: // overcurrent
+      digitalWrite(MACHINE_CTRL, 0);
+      retract(&piston_status);
+      while(1) {
+        digitalWrite(ERR_LED, 1);
+        delay(500);
+        digitalWrite(ERR_LED, 0);
+        delay(500);
+      }
+      break;
+    case 2: // low rpm
+      digitalWrite(MACHINE_CTRL, 0);
+      retract(&piston_status);
+      while(1) {
+        digitalWrite(ERR_LED, 1);
+        delay(2000);
+        digitalWrite(ERR_LED, 0);
+        delay(500);
+      }
+      break;
+    case 3: // sd card
+      while(1) {
+        digitalWrite(ERR_LED, 1);
+        delay(500);
+        digitalWrite(ERR_LED, 0);
+        delay(2000);
+      }
+      break;
+  }
 }
 
 uint8_t read_temp(uint8_t adc_ch) {
@@ -230,6 +317,8 @@ uint16_t read_rpm(uint8_t cntrl) {
   uint16_t rpm; // rpm result
   
   if(!cntrl) {
+    rpm_iter = 0; // reset iterator
+    
     local_curr_millis = millis();
     
     attachInterrupt(digitalPinToInterrupt(HALL_INT), hall_interrupt, FALLING); // reattach interrupt    
